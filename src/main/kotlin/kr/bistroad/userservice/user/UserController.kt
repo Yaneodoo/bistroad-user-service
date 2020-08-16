c2480a351fc35295109364efb468e70e8d387f12
elsiff <elsiff1002@gmail.com>
2020-08-16 17:34:50 +0900

    Add permission check on the apis

--- src/main/kotlin/kr/bistroad/userservice/user/UserController.kt
+++ src/main/kotlin/kr/bistroad/userservice/user/UserController.kt
@@ -1,11 +1,12 @@
 package kr.bistroad.userservice.user
 
+import org.springframework.security.access.prepost.PreAuthorize
 import org.springframework.web.bind.annotation.*
 import java.util.*
 
 @RestController
 class UserController(
-        private val userService: UserService
+    private val userService: UserService
 ) {
     @GetMapping("/users/{id}")
     fun getUser(@PathVariable id: UUID) = userService.readUser(id)
@@ -14,11 +15,18 @@ class UserController(
     fun getUsers(dto: UserDto.SearchReq?) = userService.searchUsers(dto)
 
     @PostMapping("/users")
+    @PreAuthorize("( #dto.role.toString() != 'ROLE_ADMIN' ) or hasRole('ROLE_ADMIN')")
     fun createUser(@RequestBody dto: UserDto.CreateReq) = userService.createUser(dto)
 
     @PatchMapping("/users/{id}")
+    @PreAuthorize(
+        "isAuthenticated() and " +
+                "(( #id == principal.userId ) or hasRole('ROLE_ADMIN')) and " +
+                "( #dto.role == null or hasRole('ROLE_ADMIN') )"
+    )
     fun patchUser(@PathVariable id: UUID, @RequestBody dto: UserDto.PatchReq) = userService.patchUser(id, dto)
 
     @DeleteMapping("/users/{id}")
+    @PreAuthorize("isAuthenticated() and (( #id == principal.userId ) or hasRole('ROLE_ADMIN'))")
     fun deleteUser(@PathVariable id: UUID) = userService.deleteUser(id)
 }
